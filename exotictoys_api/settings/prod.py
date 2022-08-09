83545a2fc0ff5f05d23756264ba0529f28ec029d
KarimovMurodilla <karimovmurodilla15@gmail.com>
2022-08-09 13:53:01 +0500

    some changes

--- exotictoys_api/settings/prod.py
+++ exotictoys_api/settings/prod.py
@@ -1,45 +1,43 @@
 import django_on_heroku
-from decouple import config
-
 from .base import *
 
-SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = False
+SECRET_KEY = env.str("SECRET_KEY")
 
-ALLOWED_HOSTS = [
-    'exotictoys.herokuapp.com'
-]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Amazon S3 Settings
+AWS_ACCESS_KEY_ID = env.str("AWS_ACCESS_KEY_ID")
 
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env.str("AWS_SECRET_ACCESS_KEY")
 
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = env.str("AWS_STORAGE_BUCKET_NAME")
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 AWS_DEFAULT_ACL = 'public-read'
 
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
 
 AWS_LOCATION = 'static'
 
 AWS_QUERYSTRING_AUTH = False
 
-AWS_HEADERS = {'Access-Control-Allow-Origin': '*'}
+AWS_HEADERS = {
+    'Access-Control-Allow-Origin': '*'
+}
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-
 
 # Heroku Logging
 
@@ -50,8 +48,8 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': '%d/%b/%Y %H:%M:%S',
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
@@ -60,7 +58,7 @@ LOGGING = {
     'handlers': {
         'console': {
             'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
+            'class': 'logging.StreamHandler',            
         },
     },
     'loggers': {
@@ -68,9 +66,8 @@ LOGGING = {
             'handlers': ['console'],
             'level': 'DEBUG',
         },
-    }
+    },
 }
-
 
 # Heroku Settings
 django_on_heroku.settings(locals(), staticfiles=False)
